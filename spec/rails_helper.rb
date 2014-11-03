3ad0a5f9f3728826f1e5bb3de0359fbf1b9cbfd1
Spike Nox <spike01@gmail.com>
2014-11-03 17:58:40 +0000

    Testing 1 2 3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'devise'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rails'
@@ -49,4 +50,5 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
 end
